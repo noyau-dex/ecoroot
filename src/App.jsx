2f3a1b7c097818802249a816472d897de4b37dec
Nikhil <jincossec@gmail.com>
2025-09-10 22:35:04 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Leaderboard from "./pages/Leaderboard";
 import ChallengesPage from './pages/ChallengesPage.jsx'
+import Home from './pages/Home.jsx'
 
 function App() {
   return (
     <>
       {/* Other routes */}
-      <ChallengesPage />
+      <Home />  
+      {/* <ChallengesPage /> */}
       {/* <Leaderboard /> */}
     </>
   );

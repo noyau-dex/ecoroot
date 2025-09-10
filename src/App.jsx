65a717fe65e33320ac6f6f8d32d2954c7f733c49
Nikhil <jincossec@gmail.com>
2025-09-10 22:52:15 +0530

    okay

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Leaderboard from "./pages/Leaderboard";
+import HomePage from "./pages/Home.jsx";
+
+function App() {
+  return (
+ 
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </Router>
+  );
+}
